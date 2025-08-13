60c987eb1269b0a189de35356f8a1c09ed512845
Arfat-err <arfatansari15398@gmail.com>
2025-08-13 14:34:34 +0530

    convert into a BEM architecture

--- script.js
+++ script.js
@@ -10,3 +10,10 @@ const removeSideBar = document.querySelector(".cross")
 removeSideBar.addEventListener("click", ()=>{
     sideBar.style.display = "none";
 })
+
+document.getElementById('downloadBtn').addEventListener('click', function () {
+  const link = document.createElement('a');
+  link.href = 'assets/Arfat-CV.pdf';
+  link.download = 'Arfat-Ansari-CV.pdf';
+  link.click();
+});
